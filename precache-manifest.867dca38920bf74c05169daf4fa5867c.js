b2aab1d4629cc348ecef08f99f86ce77f17ec65e
Zbynek Hanak <ZbynekH@kentico.com>
2019-08-06 09:05:32 +0200

    Release 0.1

---
+++ precache-manifest.867dca38920bf74c05169daf4fa5867c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23da2373e6b16928d5cd04d65767a118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ef24f6f4ddc99d4a92",
+    "url": "/static/css/main.c39508b9.chunk.css"
+  },
+  {
+    "revision": "0df2b2fd0b6cee0a07ce",
+    "url": "/static/js/2.412c13f1.chunk.js"
+  },
+  {
+    "revision": "e1ef24f6f4ddc99d4a92",
+    "url": "/static/js/main.e2be7ee8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46b1bc8e03e5b439624dde4267e1d317",
+    "url": "/static/media/icons.46b1bc8e.svg"
+  },
+  {
+    "revision": "5471f2dd5833f0c115d1ebe4af95980d",
+    "url": "/static/media/streetview-not-found.5471f2dd.png"
+  }
+]);
