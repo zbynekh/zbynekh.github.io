3b835637aa156ffe7a7d02c15f885aab5aa86f7f
zbynekh@kentico.com <Uhrd46Lk>
2019-08-22 19:49:54 +0200

    Release 0.4

---
+++ precache-manifest.56305cbf78453ccbef98ddf20c077237.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2285566a0c9ecfbc4c733dbc0648d721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc024eeabcabdd04cd8a",
+    "url": "/static/css/main.f882197e.chunk.css"
+  },
+  {
+    "revision": "6ee3445cdab5663f6771",
+    "url": "/static/js/2.7cd85510.chunk.js"
+  },
+  {
+    "revision": "dc024eeabcabdd04cd8a",
+    "url": "/static/js/main.668aa03e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad9cca363bcd91a99949ffca294bf0ce",
+    "url": "/static/media/icons.ad9cca36.svg"
+  },
+  {
+    "revision": "5471f2dd5833f0c115d1ebe4af95980d",
+    "url": "/static/media/streetview-not-found.5471f2dd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b163fcb0aa267ea0b1d2ef48caa29db9.js"
+  "/precache-manifest.56305cbf78453ccbef98ddf20c077237.js"
 );
 
 self.addEventListener('message', (event) => {
